22dd20cf99e46b994ca85f04f1e65aa47240d536
Rojan Rajbhandari <rrojan20@gmail.com>
2026-02-01 19:04:19 +0545

    chore: gen init migration

---
+++ apps/api/drizzle/20260201131856_init.sql
@@ -0,0 +1,41 @@
+CREATE TYPE "public"."booking_status" AS ENUM('PENDING', 'CONFIRMED', 'FAILED');--> statement-breakpoint
+CREATE TABLE "bookings" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"user_id" text NOT NULL,
+	"ticket_tier_id" uuid NOT NULL,
+	"quantity" integer NOT NULL,
+	"status" "booking_status" DEFAULT 'PENDING' NOT NULL,
+	"idempotency_key" text NOT NULL,
+	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
+	"updated_at" timestamp with time zone DEFAULT now() NOT NULL,
+	CONSTRAINT "bookings_idempotencyKey_unique" UNIQUE("idempotency_key")
+);
+--> statement-breakpoint
+CREATE TABLE "concerts" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"name" text NOT NULL,
+	"artist" text NOT NULL,
+	"date" timestamp with time zone NOT NULL,
+	"venue" text NOT NULL,
+	"description" text,
+	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
+	"updated_at" timestamp with time zone DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "ticket_tiers" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"concert_id" uuid NOT NULL,
+	"name" text NOT NULL,
+	"price" numeric(10, 2) NOT NULL,
+	"total_quantity" integer NOT NULL,
+	"available_quantity" integer NOT NULL,
+	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
+	"updated_at" timestamp with time zone DEFAULT now() NOT NULL,
+	CONSTRAINT "available_quantity_check" CHECK ("ticket_tiers"."available_quantity" >= 0),
+	CONSTRAINT "total_quantity_check" CHECK ("ticket_tiers"."available_quantity" <= "ticket_tiers"."total_quantity")
+);
+--> statement-breakpoint
+ALTER TABLE "bookings" ADD CONSTRAINT "bookings_ticket_tier_id_ticket_tiers_id_fk" FOREIGN KEY ("ticket_tier_id") REFERENCES "public"."ticket_tiers"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "ticket_tiers" ADD CONSTRAINT "ticket_tiers_concert_id_concerts_id_fk" FOREIGN KEY ("concert_id") REFERENCES "public"."concerts"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+CREATE INDEX "bookings_user_id_idx" ON "bookings" USING btree ("user_id");--> statement-breakpoint
+CREATE INDEX "bookings_ticket_tier_id_idx" ON "bookings" USING btree ("ticket_tier_id");
